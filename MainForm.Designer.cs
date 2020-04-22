13d974f1f9d8f358cbd1f83ef2ce7ab4ce04daff
James Atkin <curlyw42@gmail.com>
2020-04-22 20:51:41 +0100

    Added about page

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.materialListView1 = new MaterialSkin.Controls.MaterialListView();
             this.matTabCtrl = new MaterialSkin.Controls.MaterialTabControl();
             this.tabPgInstalled = new System.Windows.Forms.TabPage();
@@ -50,9 +51,11 @@
             this.materialDivider1 = new MaterialSkin.Controls.MaterialDivider();
             this.lblNumMapsInstalledHeader = new MaterialSkin.Controls.MaterialLabel();
             this.lblNumMapsInstalled = new MaterialSkin.Controls.MaterialLabel();
+            this.richTxtAbout = new System.Windows.Forms.RichTextBox();
             this.matTabCtrl.SuspendLayout();
             this.tabPgInstalled.SuspendLayout();
             this.tabPgSettings.SuspendLayout();
+            this.tabPgAbout.SuspendLayout();
             this.SuspendLayout();
             // 
             // materialListView1
@@ -84,19 +87,22 @@
             this.matTabCtrl.MouseState = MaterialSkin.MouseState.HOVER;
             this.matTabCtrl.Name = "matTabCtrl";
             this.matTabCtrl.SelectedIndex = 0;
-            this.matTabCtrl.Size = new System.Drawing.Size(1078, 533);
+            this.matTabCtrl.Size = new System.Drawing.Size(1406, 533);
             this.matTabCtrl.TabIndex = 5;
             this.matTabCtrl.SelectedIndexChanged += new System.EventHandler(this.matTabCtrl_SelectedIndexChanged);
             // 
             // tabPgInstalled
             // 
+            this.tabPgInstalled.BackColor = System.Drawing.Color.Transparent;
+            this.tabPgInstalled.Controls.Add(this.lblNumMapsInstalled);
             this.tabPgInstalled.Controls.Add(this.lstVwInstalledMaps);
+            this.tabPgInstalled.Controls.Add(this.lblNumMapsInstalledHeader);
+            this.tabPgInstalled.Controls.Add(this.materialDivider1);
             this.tabPgInstalled.Location = new System.Drawing.Point(4, 22);
             this.tabPgInstalled.Name = "tabPgInstalled";
-            this.tabPgInstalled.Size = new System.Drawing.Size(1070, 507);
+            this.tabPgInstalled.Size = new System.Drawing.Size(1398, 507);
             this.tabPgInstalled.TabIndex = 0;
             this.tabPgInstalled.Text = "Installed maps";
-            this.tabPgInstalled.UseVisualStyleBackColor = true;
             // 
             // lstVwInstalledMaps
             // 
@@ -107,15 +113,15 @@
             this.colAuthor,
             this.colDownloadLink});
             this.lstVwInstalledMaps.Depth = 0;
-            this.lstVwInstalledMaps.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.lstVwInstalledMaps.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.lstVwInstalledMaps.FullRowSelect = true;
             this.lstVwInstalledMaps.HideSelection = false;
-            this.lstVwInstalledMaps.Location = new System.Drawing.Point(8, 3);
+            this.lstVwInstalledMaps.Location = new System.Drawing.Point(18, 21);
             this.lstVwInstalledMaps.MouseLocation = new System.Drawing.Point(-1, -1);
             this.lstVwInstalledMaps.MouseState = MaterialSkin.MouseState.OUT;
             this.lstVwInstalledMaps.Name = "lstVwInstalledMaps";
             this.lstVwInstalledMaps.OwnerDraw = true;
-            this.lstVwInstalledMaps.Size = new System.Drawing.Size(1305, 495);
+            this.lstVwInstalledMaps.Size = new System.Drawing.Size(1038, 477);
             this.lstVwInstalledMaps.Sorting = System.Windows.Forms.SortOrder.Ascending;
             this.lstVwInstalledMaps.TabIndex = 0;
             this.lstVwInstalledMaps.UseCompatibleStateImageBehavior = false;
@@ -139,7 +145,7 @@
             // colDownloadLink
             // 
             this.colDownloadLink.Text = "Download link";
-            this.colDownloadLink.Width = 405;
+            this.colDownloadLink.Width = 470;
             // 
             // tabPgGet
             // 
@@ -162,7 +168,7 @@
             this.tabPgSettings.Controls.Add(this.btnSettingsSave);
             this.tabPgSettings.Location = new System.Drawing.Point(4, 22);
             this.tabPgSettings.Name = "tabPgSettings";
-            this.tabPgSettings.Size = new System.Drawing.Size(776, 434);
+            this.tabPgSettings.Size = new System.Drawing.Size(1398, 507);
             this.tabPgSettings.TabIndex = 2;
             this.tabPgSettings.Text = "Settings";
             // 
@@ -171,7 +177,7 @@
             this.rdBtnDark.AutoSize = true;
             this.rdBtnDark.Depth = 0;
             this.rdBtnDark.Font = new System.Drawing.Font("Roboto", 10F);
-            this.rdBtnDark.Location = new System.Drawing.Point(8, 172);
+            this.rdBtnDark.Location = new System.Drawing.Point(18, 181);
             this.rdBtnDark.Margin = new System.Windows.Forms.Padding(0);
             this.rdBtnDark.MouseLocation = new System.Drawing.Point(-1, -1);
             this.rdBtnDark.MouseState = MaterialSkin.MouseState.HOVER;
@@ -189,7 +195,7 @@
             this.lblSettingsTheme.Depth = 0;
             this.lblSettingsTheme.Font = new System.Drawing.Font("Roboto", 11F);
             this.lblSettingsTheme.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.lblSettingsTheme.Location = new System.Drawing.Point(7, 101);
+            this.lblSettingsTheme.Location = new System.Drawing.Point(17, 110);
             this.lblSettingsTheme.MouseState = MaterialSkin.MouseState.HOVER;
             this.lblSettingsTheme.Name = "lblSettingsTheme";
             this.lblSettingsTheme.Size = new System.Drawing.Size(55, 19);
@@ -201,7 +207,7 @@
             this.rdBtnLight.AutoSize = true;
             this.rdBtnLight.Depth = 0;
             this.rdBtnLight.Font = new System.Drawing.Font("Roboto", 10F);
-            this.rdBtnLight.Location = new System.Drawing.Point(8, 130);
+            this.rdBtnLight.Location = new System.Drawing.Point(18, 139);
             this.rdBtnLight.Margin = new System.Windows.Forms.Padding(0);
             this.rdBtnLight.MouseLocation = new System.Drawing.Point(-1, -1);
             this.rdBtnLight.MouseState = MaterialSkin.MouseState.HOVER;
@@ -219,7 +225,7 @@
             this.btnSettingsBrowse.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnSettingsBrowse.Depth = 0;
             this.btnSettingsBrowse.Icon = null;
-            this.btnSettingsBrowse.Location = new System.Drawing.Point(460, 44);
+            this.btnSettingsBrowse.Location = new System.Drawing.Point(674, 53);
             this.btnSettingsBrowse.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnSettingsBrowse.Name = "btnSettingsBrowse";
             this.btnSettingsBrowse.Primary = true;
@@ -233,8 +239,10 @@
             // 
             this.txtSettingsAddon.BackColor = System.Drawing.SystemColors.Control;
             this.txtSettingsAddon.Depth = 0;
+            this.txtSettingsAddon.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.txtSettingsAddon.ForeColor = System.Drawing.SystemColors.ControlText;
             this.txtSettingsAddon.Hint = "";
-            this.txtSettingsAddon.Location = new System.Drawing.Point(8, 44);
+            this.txtSettingsAddon.Location = new System.Drawing.Point(18, 53);
             this.txtSettingsAddon.MaxLength = 32767;
             this.txtSettingsAddon.MouseState = MaterialSkin.MouseState.HOVER;
             this.txtSettingsAddon.Name = "txtSettingsAddon";
@@ -242,7 +250,7 @@
             this.txtSettingsAddon.SelectedText = "";
             this.txtSettingsAddon.SelectionLength = 0;
             this.txtSettingsAddon.SelectionStart = 0;
-            this.txtSettingsAddon.Size = new System.Drawing.Size(425, 23);
+            this.txtSettingsAddon.Size = new System.Drawing.Size(640, 23);
             this.txtSettingsAddon.TabIndex = 9;
             this.txtSettingsAddon.TabStop = false;
             this.txtSettingsAddon.UseSystemPasswordChar = false;
@@ -253,7 +261,7 @@
             this.lblSettingsAddon.Depth = 0;
             this.lblSettingsAddon.Font = new System.Drawing.Font("Roboto", 11F);
             this.lblSettingsAddon.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.lblSettingsAddon.Location = new System.Drawing.Point(8, 14);
+            this.lblSettingsAddon.Location = new System.Drawing.Point(18, 23);
             this.lblSettingsAddon.MouseState = MaterialSkin.MouseState.HOVER;
             this.lblSettingsAddon.Name = "lblSettingsAddon";
             this.lblSettingsAddon.Size = new System.Drawing.Size(111, 19);
@@ -266,7 +274,7 @@
             this.btnSettingsSave.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnSettingsSave.Depth = 0;
             this.btnSettingsSave.Icon = null;
-            this.btnSettingsSave.Location = new System.Drawing.Point(8, 234);
+            this.btnSettingsSave.Location = new System.Drawing.Point(18, 243);
             this.btnSettingsSave.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnSettingsSave.Name = "btnSettingsSave";
             this.btnSettingsSave.Primary = true;
@@ -278,9 +286,10 @@
             // 
             // tabPgAbout
             // 
+            this.tabPgAbout.Controls.Add(this.richTxtAbout);
             this.tabPgAbout.Location = new System.Drawing.Point(4, 22);
             this.tabPgAbout.Name = "tabPgAbout";
-            this.tabPgAbout.Size = new System.Drawing.Size(776, 434);
+            this.tabPgAbout.Size = new System.Drawing.Size(1398, 507);
             this.tabPgAbout.TabIndex = 3;
             this.tabPgAbout.Text = "About";
             this.tabPgAbout.UseVisualStyleBackColor = true;
@@ -301,7 +310,7 @@
             // 
             this.materialDivider1.BackColor = System.Drawing.SystemColors.Control;
             this.materialDivider1.Depth = 0;
-            this.materialDivider1.Location = new System.Drawing.Point(1117, 152);
+            this.materialDivider1.Location = new System.Drawing.Point(1109, 3);
             this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialDivider1.Name = "materialDivider1";
             this.materialDivider1.Size = new System.Drawing.Size(286, 244);
@@ -311,10 +320,11 @@
             // lblNumMapsInstalledHeader
             // 
             this.lblNumMapsInstalledHeader.AutoSize = true;
+            this.lblNumMapsInstalledHeader.BackColor = System.Drawing.SystemColors.Control;
             this.lblNumMapsInstalledHeader.Depth = 0;
             this.lblNumMapsInstalledHeader.Font = new System.Drawing.Font("Roboto", 11F);
             this.lblNumMapsInstalledHeader.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.lblNumMapsInstalledHeader.Location = new System.Drawing.Point(1131, 167);
+            this.lblNumMapsInstalledHeader.Location = new System.Drawing.Point(1126, 21);
             this.lblNumMapsInstalledHeader.MouseState = MaterialSkin.MouseState.HOVER;
             this.lblNumMapsInstalledHeader.Name = "lblNumMapsInstalledHeader";
             this.lblNumMapsInstalledHeader.Size = new System.Drawing.Size(186, 19);
@@ -324,24 +334,32 @@
             // lblNumMapsInstalled
             // 
             this.lblNumMapsInstalled.AutoSize = true;
+            this.lblNumMapsInstalled.BackColor = System.Drawing.SystemColors.Window;
             this.lblNumMapsInstalled.Depth = 0;
             this.lblNumMapsInstalled.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold);
             this.lblNumMapsInstalled.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.lblNumMapsInstalled.Location = new System.Drawing.Point(1131, 188);
+            this.lblNumMapsInstalled.Location = new System.Drawing.Point(1127, 52);
             this.lblNumMapsInstalled.MouseState = MaterialSkin.MouseState.HOVER;
             this.lblNumMapsInstalled.Name = "lblNumMapsInstalled";
             this.lblNumMapsInstalled.Size = new System.Drawing.Size(17, 18);
             this.lblNumMapsInstalled.TabIndex = 11;
             this.lblNumMapsInstalled.Text = "0";
             // 
+            // richTxtAbout
+            // 
+            this.richTxtAbout.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTxtAbout.Location = new System.Drawing.Point(8, 12);
+            this.richTxtAbout.Name = "richTxtAbout";
+            this.richTxtAbout.ReadOnly = true;
+            this.richTxtAbout.Size = new System.Drawing.Size(770, 198);
+            this.richTxtAbout.TabIndex = 2;
+            this.richTxtAbout.Text = resources.GetString("richTxtAbout.Text");
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1440, 659);
-            this.Controls.Add(this.lblNumMapsInstalled);
-            this.Controls.Add(this.lblNumMapsInstalledHeader);
-            this.Controls.Add(this.materialDivider1);
+            this.ClientSize = new System.Drawing.Size(1440, 691);
             this.Controls.Add(this.matTabSel);
             this.Controls.Add(this.matTabCtrl);
             this.Controls.Add(this.materialListView1);
@@ -349,10 +367,11 @@
             this.Text = "Womp Rat | SWBFII Map Manager";
             this.matTabCtrl.ResumeLayout(false);
             this.tabPgInstalled.ResumeLayout(false);
+            this.tabPgInstalled.PerformLayout();
             this.tabPgSettings.ResumeLayout(false);
             this.tabPgSettings.PerformLayout();
+            this.tabPgAbout.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -379,6 +398,7 @@
         private MaterialSkin.Controls.MaterialDivider materialDivider1;
         private MaterialSkin.Controls.MaterialLabel lblNumMapsInstalledHeader;
         private MaterialSkin.Controls.MaterialLabel lblNumMapsInstalled;
+        private System.Windows.Forms.RichTextBox richTxtAbout;
     }
 }
 
